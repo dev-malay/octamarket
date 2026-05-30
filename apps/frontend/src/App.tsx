908f3d96b87e10b049b1075c1ea0ff764ef986f6
dev-malay <malayworkz@gmail.com>
2026-05-30 21:21:40 +0530

    refactor web3 auth

--- apps/frontend/src/App.tsx
+++ apps/frontend/src/App.tsx
@@ -1,6 +1,36 @@
+import { useSupabase } from "./hooks/useSupabase";
+import { useUser } from "./hooks/useUser";
+
+
 function App() {
-    return <div>t</div>
-  }
+
+  const { claims } = useUser()
+  const supabase = useSupabase()
+
+
+    return <div>{ window.phantom?.solana && !claims && 
+      <button onClick={async() => { 
+        try {
+          if (!window.phantom?.solana?.publicKey) {
+            await window.phantom?.solana?.connect();
+          }
+          await supabase.auth.signInWithWeb3({
+            chain: 'solana',
+            statement: 'I want to sign in with Solana. Please connect wallet',
+            wallet: window.phantom?.solana
+          });
+        } catch (err) {
+          console.error("Wallet sign-in failed:", err);
+        }
+      }}>signin with solana</button>}
+
+      {claims && <button onClick={async () => { await supabase.auth.signOut()}}>logout</button>}
+
+      {JSON.stringify(claims)}
+    </div>
+
+
+  }  
 
 
 export default App;
