db15f4392065084b9971a227ec4512df7f38bacf
dev-malay <malayworkz@gmail.com>
2026-05-31 10:25:33 +0530

    refactor auth middleware

--- apps/backend/middleware.ts
+++ apps/backend/middleware.ts
@@ -1,10 +1,39 @@
 import type { NextFunction, Request, Response } from "express";
+import { prisma } from "db";
+import { createClient } from "@supabase/supabase-js";
+const supabase = createClient("https://tiqwzsehcnqiypxonwkp.supabase.co", process.env.SUPABASE_SECRET_KEY)
+
 
 export async function middleware(req: Request, res: Response, next: NextFunction) {
     const token = req.header.authorization;
-
-    const { data: {user}, error} = await supabase.auth.getUser(token) 
- 
+      try {
+        const { data: { user }, error } = await supabase.auth.getUser(token);
+        const address: string = user?.user_metadata.custom_claims.address;
+        const userDb = await prisma.user.upsert({
+            where: {
+                address,
+            },
+            update: {
+                address,
+            },
+            create: {
+                address,
+                usdBalance: 0
+            }
+        })
+        if (address) {
+            req.userId = userDb.id;
+            next();
+        } else {
+            res.status(403).json({
+                message: "Incorrect credentials"
+            })
+        }
+    } catch(e) {
+        res.status(403).json({
+            message: "Incorrect credentials"
+        })
+    }
 
 
 }   
